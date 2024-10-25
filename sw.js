099703d34713b3338213efeb946fa33493e35a1b
jonatanLara <laraortizjonatan@gmail.com>
2024-10-25 11:20:00 -0600

    Modificanciones de iphone x+up

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 //imports
 importScripts('js/sw-utils.js')
 
-const STATIC_CACHE = 'static-v2'
+const STATIC_CACHE = 'static-v1'
 const DYNAMIC_CACHE = 'dynamic-v1'
 const INMUTABLE_CACHE = 'inmutable-v1'
 
@@ -38,6 +38,9 @@ self.addEventListener('activate', e => {
     const respuesta = caches.keys().then(keys => {
         keys.forEach(key => {
             if (STATIC_CACHE !== key && key.includes('static')) {
+                caches.delete(key)
+            }
+            if (DYNAMIC_CACHE !== key && key.includes('dynamic')) {
                 caches.delete(key)
             }
         })
